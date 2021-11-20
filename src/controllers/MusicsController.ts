527563ff97299177e9656d8dc1cea8c656e9704f
souz4s <psouza.1650@gmail.com>
2021-11-20 01:52:58 -0300

    added MusicsController class with CRUD methods and routes

---
+++ src/controllers/MusicsController.ts
@@ -0,0 +1,56 @@
+import PrismaConfig from '../config/prisma';
+import { Request, Response } from 'express'
+
+export default class MusicsController {
+    public prisma = new PrismaConfig().prisma;
+
+    public async index(req: Request, res: Response) {
+        const musics = await this.prisma.musics.findMany()
+
+        return res.send(musics)
+    }
+
+    public async store(req: Request, res: Response) {
+        const data = req.body
+
+        const music = await this.prisma.musics.create({
+            data: data
+        })
+
+        return res.status(200).send(music)
+    }
+
+    public async show(req: Request, res: Response) {
+        const musicId = req.params.musicId
+        const music = await this.prisma.musics.findUnique({
+            where: {
+                id: parseInt(musicId.toString())
+            }
+        })
+
+        return res.status(200).send(music)
+    }
+
+    public async update(req: Request, res: Response) {
+        const data = req.body
+        const musicId = req.params.musicId
+        const music = await this.prisma.musics.update({
+            where: {
+                id: parseInt(musicId.toString())
+            },
+            data: data
+        })
+
+        return res.status(200).send(music)
+    }
+
+    public async destroy(req: Request, res: Response) {
+        const musicId = req.params.musicId
+
+        return res.status(200).send(await this.prisma.musics.delete({
+            where: {
+                id: parseInt(musicId.toString())
+            }
+        }))
+    }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express'
 
+import MusicsController from '../controllers/MusicsController'
+
 const router = Router()
+
+const musicsController = new MusicsController()
 
 router.get('/hello', (req, res) => {
     var date = new Date()
     return res.json({ date: date.toString() })
 })
+
+/**
+ * Musics Routes
+ */
+
+router.get('/music', (req, res) => musicsController.index(req, res))
+router.post('/music', (req, res) => musicsController.store(req, res))
+router.get('/music/:musicId', (req, res) => musicsController.show(req, res))
+router.put('/music/:musicId', (req, res) => musicsController.update(req, res))
+router.delete('/music/:musicId', (req, res) => musicsController.destroy(req, res))
 
 module.exports = router;
